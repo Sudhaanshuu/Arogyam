c4b424dfad7a95ad2f5e38ca11397fea2c7e4539
Sudhanshu <2201030100@cgu-odisha.ac.in>
2024-01-10 23:40:04 +0530

    Add pages functions for the sign up

--- Extra/js/script.js
+++ Extra/js/script.js
@@ -1,6 +1,7 @@
 // For the floating login page
 const loginModal = document.getElementById("login-modal");
 const closeModal = document.getElementById("close-modal");
+const newlog = document.getElementsByClassName("sign")
 
 window.addEventListener("load", () => {
     loginModal.style.display = "block";
