f04977be038cd8420b813e497176553b7431d3f8
Sudhanshu <2201030100@cgu-odisha.ac.in>
2024-01-16 15:42:03 +0530

    Remove some of the files

--- Extra/js/script.js
+++ Extra/js/script.js
@@ -1,7 +1,6 @@
 // For the floating login page
 const loginModal = document.getElementById("login-modal");
 const closeModal = document.getElementById("close-modal");
-const grid = document.getElementById("grid: ")
 const newlog = document.getElementsByClassName("sign")
 
 window.addEventListener("load", () => {
